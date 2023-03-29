7d475e9039b16269e78c8c2817509af40da88046
Yeizer Marrugo <yeizermarrugo@gmail.com>
2023-03-29 11:56:52 -0500

    [ADD] controller added

--- src/starup/container.js
+++ src/starup/container.js
@@ -9,7 +9,7 @@ const app= require('.')
 const {HomeService, UserService, IdeaService, CommentService} = require('../services')
 
 //controllers
-const{HomeController}= require('../controllers')
+const{HomeController, UserController, IdeaController, CommentController}= require('../controllers')
 
 //routes
 const {HomeRoutes} = require('../routes/index.routes')
@@ -37,7 +37,10 @@ container
     CommentService: asClass(CommentService).singleton(),
     IdeaService: asClass(IdeaService).singleton()
 }).register({
-    HomeController: asClass(HomeController.bind(HomeController)).singleton()
+    HomeController: asClass(HomeController.bind(HomeController)).singleton(),
+    UserController: asClass(UserController.bind(UserController)).singleton(),
+    CommentController: asClass(CommentController.bind(CommentController)).singleton(),
+    IdeaController: asClass(IdeaController.bind(IdeaController)).singleton()
 }).register({
     HomeRoutes: asFunction(HomeRoutes).singleton()
 }).register({
