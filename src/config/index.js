14139777db10425be3ac23d5ee5ac77aed344435
Yeizer Marrugo <yeizermarrugo@gmail.com>
2023-03-29 15:17:08 -0500

    [ADD] jwt key added

--- src/config/index.js
+++ src/config/index.js
@@ -5,5 +5,6 @@ if(process.env.NODE_ENV !== 'production'){
 module.exports = {
     PORT: process.env.PORT,
     MONGO_URL: process.env.MONGO_URL,
-    APPLICATION_NAME: process.env.APPLICATION_NAME
+    APPLICATION_NAME: process.env.APPLICATION_NAME,
+    JWT_SECRET: process.env.JWT_SECRET
 }
