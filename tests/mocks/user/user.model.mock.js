9946da7ee9d95da3587df50b806ead5aaa5804c8
Yeizer Marrugo <yeizermarrugo@gmail.com>
2023-03-30 01:24:01 -0500

    [ADD] tests added

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,32 @@
+module.exports = {
+    user:   {
+        "_id": "642494b062f69a1d769844e8",
+        "name": "Yeizer",
+        "username": "Yemave",
+        "__v": 0
+    },
+    users:[ {
+        "_id": "6424a0880c682e268563ee67",
+        "name": "Edith",
+        "username": "Edvepe",
+        "__v": 0
+    },
+    {
+        "_id": "6424a0940c682e268563ee6a",
+        "name": "Rene",
+        "username": "Remaca",
+        "__v": 0
+    },
+    {
+        "_id": "6424a0a40c682e268563ee6d",
+        "name": "Laura",
+        "username": "Lala",
+        "__v": 0
+    },
+    {
+        "_id": "6424a0e60c682e268563ee72",
+        "name": "Yesid",
+        "username": "Yerry",
+        "__v": 0
+    }]
+}
