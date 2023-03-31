365690fc797164a7b098c1efac174f9916900cd5
Yeizer Marrugo <yeizermarrugo@gmail.com>
2023-03-31 12:37:21 -0500

    [ADD] swagger implemented

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 require('express-async-errors');
-const {NotFoundMiddlware, ErrorMiddlware} = require('../middlewares')
+const {NotFoundMiddlware, ErrorMiddlware} = require('../middlewares');
+const swaggerUI = require('swagger-ui-express');
+const {SWAGGER_PATH} = require('../config');
+const swaggerDocument = require(SWAGGER_PATH)
 
 
 module.exports = function({HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes, AuthRoutes}){
@@ -23,8 +26,9 @@ module.exports = function({HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes, Au
     apiRoutes.use("/auth", AuthRoutes);
 
 
-    router.use("/v1/api", apiRoutes);
 
+    router.use("/v1/api", apiRoutes);
+    router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
     //Rutas para manejar errores
     router.use(NotFoundMiddlware);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,10 +5,16 @@ const {CACHE_TIME} = require("../helpers")
 module.exports = function ({UserController}){
     const router = Router();
 
-    router.get('', [AuthMiddlware, ParseIntMiddleware, CacheMiddleware(CACHE_TIME.ONE_HOUR)], UserController.getAll);
+    router.get('',
+        [
+        AuthMiddlware,
+        ParseIntMiddleware,
+        CacheMiddleware(CACHE_TIME.ONE_HOUR)
+        ],
+        UserController.getAll);
     router.get('/:userId', UserController.get);
-    router.patch('/:userId', UserController.update);
-    router.delete('/:userId', UserController.delete);
+    router.patch('/:userId', AuthMiddlware, UserController.update);
+    router.delete('/:userId', AuthMiddlware, UserController.delete);
 
     return router;
 }
